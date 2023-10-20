656da66c4833dd9b56bf27024665c3ffb968c346
Bolorchuluun <bolorcln@gmail.com>
2023-10-20 15:04:57 +0800

    add exp and jti

--- app/Http/Controllers/TableauServerController.php
+++ app/Http/Controllers/TableauServerController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\TableauServerConfig;
+use Carbon\Carbon;
 use Illuminate\Http\Response;
 use Firebase\JWT\JWT;
+use Symfony\Component\Uid\UuidV4;
+use Webpatser\Uuid\Uuid;
 
 class TableauServerController extends Controller
 {
@@ -18,8 +21,8 @@ class TableauServerController extends Controller
         $key = $serverConfig->client_secret;
         $payload = [
             'iss' => $serverConfig->client_id,
-            'exp' => '',
-            'jti' => '',
+            'exp' => Carbon::now()->addMinutes(5)->getTimestamp(),
+            'jti' => Uuid::generate(4)->string,
             'aud' => 'tableau',
             'sub' => $serverConfig->access_name,
             'scp' => [
